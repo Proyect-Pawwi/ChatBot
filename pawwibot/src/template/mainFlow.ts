3521f56d2e79945beb48d3ad43b4218668a8fe55
JuanDiegoEcheverryP <juan_echeverry@javeriana.edu.co>
2025-09-02 11:34:15 -0500

    mongoTest

--- pawwibot/src/template/mainFlow.ts
+++ pawwibot/src/template/mainFlow.ts
@@ -1240,7 +1240,7 @@ const checkLeadsMongo = async () => {
   }
 };
 
-/*
+
 setTimeout(() => {
   setInterval(checkLeadsMongo, 5 * 1000);
 }, 5000);
@@ -1252,5 +1252,5 @@ setTimeout(() => {
 setTimeout(() => {
   setInterval(revisarFinalizacionPaseos, 8 * 1000);
 }, 5000);
-*/
+
 export { init, RegistrarNombrePerrito, RegistrarRazaPerrito, RegistrarEdadPerrito, RegistrarConsideracionesPerrito, RegistrarVacunasPerrito, RegistrarDireccion, RegistrarPerro, AgendarlistarPerritos, agendarTiempoPaseo, agendarDiaPaseo, agendarHoraPaseo, agendarMetodoPaseo, agendarResumenPaseo};
