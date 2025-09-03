dd596199050c949b165f44dc59c1e001113bf0bc
JuanDiegoEcheverryP <juan_echeverry@javeriana.edu.co>
2025-09-02 19:48:23 -0500

    sasa

--- pawwibot/src/template/mainFlow.ts
+++ pawwibot/src/template/mainFlow.ts
@@ -629,7 +629,7 @@ const init = addKeyword(EVENTS.WELCOME)
 
             if (paseo.Estado == "Esperando Pawwer") {
               if (ctx.payload !== "confirmar_llegada") {
-                await TEMPLATE_llegada_pawwer(ctx.from, { nombrePawwer:pawwer.Nombre, nombrePerrito });
+                await TEMPLATE_llegada_pawwer(ctx.from, { nombrePawwer:pawwer.Nombre, nombrePerrito:paseo.Perro });
                 return endFlow();
               }
               
