f3cf6bf9003ce4a5d4e787a8d42b90230e08cc7a
JuanDiegoEcheverryP <juan_echeverry@javeriana.edu.co>
2025-10-11 00:37:17 -0500

    mini

--- pawwibot/src/services/mongoDB/mongo-paseos.ts
+++ pawwibot/src/services/mongoDB/mongo-paseos.ts
@@ -172,6 +172,14 @@ export async function actualizarEstadoPaseosProximos() {
       const horaFin = DateTime.fromFormat(paseo.horaFin,"yyyy-MM-dd HH:mm:ss",{ zone: "America/Bogota" });
       const diffMinutos = DateTime.now().setZone("America/Bogota").diff(horaFin, "minutes").minutes;
 
+      if (paseo.MetodoPago != "Efectivo") {
+        await TEMPLATE_recordatorio_pago_cliente(paseo.Celular, {
+          nombreCliente: paseo.Nombre,
+          nombrePerrito: paseo.Perro,
+          valorPaseo: paseo.Precio?.toString() || "No definido"
+        });
+      }
+
       if (diffMinutos > 15) {
         console.log("⚠️ Ya pasaron más de 15 minutos desde la hora de finalización.");
 
@@ -199,14 +207,6 @@ export async function actualizarEstadoPaseosProximos() {
           idPawwer: paseo.IdPawwer ?? paseo.idPawwer ?? null,
         };
         await moverPaseoACompletados(paseoMapped);
-
-        if (paseo.MetodoPago != "Efectivo") {
-          await TEMPLATE_recordatorio_pago_cliente(paseo.Celular, {
-            nombreCliente: paseo.Nombre,
-            nombrePerrito: paseo.Perro,
-            valorPaseo: paseo.Precio?.toString() || "No definido"
-          });
-        }
       }
     }
 
