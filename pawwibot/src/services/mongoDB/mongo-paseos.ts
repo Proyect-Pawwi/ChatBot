e959af3e2efb2509c1f49050b08148e640428e43
JuanDiegoEcheverryP <juan_echeverry@javeriana.edu.co>
2025-10-22 22:47:27 -0500

    completado

--- pawwibot/src/services/mongoDB/mongo-paseos.ts
+++ pawwibot/src/services/mongoDB/mongo-paseos.ts
@@ -167,12 +167,12 @@ export async function actualizarEstadoPaseosProximos() {
     const pawwerActivo = await pawwerActivoCol.findOne({ _id: new ObjectId(paseo.pawwer) });
 
     //Paseo ultimo mensaje si han pasado mas de 15 minutos de la hora de finalizacion
-    if (paseo.Estado === "Completado (15 minutos recordatorio de pago)" && paseo.horaFin) {
+    if (paseo.Estado === "Completado (15 minutos recordatorio de pago)" && paseo.horaFin || paseo.estado === "Completado") {
 
       const horaFin = DateTime.fromFormat(paseo.horaFin,"yyyy-MM-dd HH:mm:ss",{ zone: "America/Bogota" });
       const diffMinutos = DateTime.now().setZone("America/Bogota").diff(horaFin, "minutes").minutes;
 
-      if (diffMinutos > 15 || paseo.MetodoPago === "Efectivo") {
+      if (diffMinutos > 15 || paseo.MetodoPago === "Efectivo" || paseo.estado === "Completado") {
         console.log("⚠️ Ya pasaron más de 15 minutos desde la hora de finalización.");
 
         // Map paseo fields from DB (camelCase) to Paseo interface (PascalCase)
